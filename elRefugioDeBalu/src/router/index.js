b1a2754d7bc6153d2fea366d7e85b7825d8d97fc
Marianne Santos <20213tn140@utez.edu.mx>
2024-01-23 13:19:56 -0600

    router, bootstrap

---
+++ elRefugioDeBalu/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/home",
+  },
+  {
+    path: "/",
+    component: {
+      render(c) {
+        return c("router-view");
+      },
+    },
+    children: [
+      {
+        path: "/home",
+        name: "home",
+        component: () => import("../views/Home.vue"),
+      }
+    ],
+  },
+];
+
+const router = new VueRouter({ routes });
+export default router;
---
+++ elRefugioDeBalu/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <b-container fluid>
+        <b-row>
+            <b-col cols="12" class="text-center mt-5">
+                <h1>Home</h1>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
