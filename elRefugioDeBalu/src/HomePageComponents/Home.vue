0fef088baba33e80e73cb69fab14b6494c8bcce4
AlixStart313 <20213tn014@utez.edu.mx>
2024-02-06 14:50:25 -0600

    [MODIFY] carrusel y estilos css

--- elRefugioDeBalu/src/HomePageComponents/Home.vue
+++ elRefugioDeBalu/src/HomePageComponents/Home.vue
@@ -1,9 +1,9 @@
 <template>
     <b-container fluid>
         <b-row>
-            <b-col cols="12" class="text-center mt-5">
-                <b-row class="ps-4 pe-4">
-                    <b-col class="header-homePage ms-5  shadow">
+            <b-col cols="12" class="mt-5">
+                <b-row class="px-5">
+                    <b-col class="header-homePage ms-5 shadow">
                         <h1 class="title-headerPage mt-2 pe-0 text-end">
                             Adopta amor,
                         </h1>
@@ -14,44 +14,12 @@
                         </h1>
                     </b-col>
                 </b-row>
-                <b-row>
-                    <b-col class="mt-4">
-                        <b-container fluid class="py-2 px-5">
-                            <b-carousel class="mx-3" id="carousel-1" v-model="slide" :interval="4000" 
-                                background="#ababab" img-width="1024" img-height="480"
-                                style="text-shadow: 1px 1px 2px #333;">
-                                <!-- Text slides with image -->
-                                <b-carousel-slide caption="First slide"
-                                    text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-                                    img-src="https://picsum.photos/1024/480/?image=52"></b-carousel-slide>
-
-                                <!-- Slides with custom text -->
-                                <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-                                    <h1>Hello world!</h1>
-                                </b-carousel-slide>
-
-                                <!-- Slides with image only -->
-                                <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-                                <!-- Slides with img slot -->
-                                <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-                                <b-carousel-slide>
-                                    <template #img>
-                                        <img class="d-block img-fluid w-100" width="1024" height="480"
-                                            src="https://picsum.photos/1024/480/?image=55" alt="image slot">
-                                    </template>
-                                </b-carousel-slide>
-
-                                <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-                                <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis,
-                                        tincidunt
-                                        a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-                                    </p>
-                                </b-carousel-slide>
-                            </b-carousel>
+                <b-row class="px-4 mt-2 text-center">
+                    <b-col class="mx-5">
+                        <b-container>
+                            <CarouselGalery />
                         </b-container>
+
                     </b-col>
                 </b-row>
                 <b-row class="mt-3 ps-3 pe-3">
@@ -99,10 +67,10 @@
                 </b-row>
                 <b-row class="mt-3 mb-4 mx-5 px-3">
                     <b-col class="info-homepage">
-                       <div class="title-headerPage mt-5 mb-3 text-center">
+                        <div class="title-headerPage mt-5 mb-3 text-center">
                             <h4>Tu compañero ideal está esperando: descubre los amigos que tenemos para ti.</h4>
                             <b-button>Vamos a conocerlos</b-button>
-                       </div>
+                        </div>
                     </b-col>
                 </b-row>
             </b-col>
@@ -111,8 +79,11 @@
 </template>
 
 <script>
+import CarouselGalery from '../HomePageComponents/Carrorusel.vue'
 export default {
-
+    components: {
+        CarouselGalery
+    }
 }
 </script>
 
