70cbd175c7b349f19a790d3be6a8ff3ae380fb96
AlixStart313 <20213tn014@utez.edu.mx>
2024-02-07 17:24:37 -0600

    avacne carrusel

---
+++ elRefugioDeBalu/src/HomePageComponents/Banner.vue
@@ -0,0 +1,17 @@
+<template>
+    <figure class="Banner">
+        <img src="../../src/assets/imgs/tortuga.jpg" alt="Villa Verde preview">
+        <figcaption class="carrusel py-5 text-center">
+            <h3 class="textGalery">Adopta, no compres: una decisión que salva vidas</h3>
+        </figcaption>
+    </figure>
+</template>
+  
+<script>
+export default {
+    name: "Banner",
+}
+</script>
+
+<style></style>
+  
