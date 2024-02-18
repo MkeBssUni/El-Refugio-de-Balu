ef72a2f61f6142124cedbf26b62852ee7ed81745
AlixStart313 <20213tn014@utez.edu.mx>
2024-02-18 13:47:00 -0600

    [MODIFY] pagina de animales y landing page

--- elRefugioDeBalu/src/HomePageComponents/Home.vue
+++ elRefugioDeBalu/src/HomePageComponents/Home.vue
@@ -1,63 +1,54 @@
 <template>
-    <b-container fluid class="border">
-        <b-row>
-            <b-col cols="12" class="mt-5">
-                <b-row class="justify-content-center border">
-                    <b-col class="header-homePage shadow" cols="5">
-                        <h1 class="title-headerPage mt-2 pe-0 text-end">
-                            Adopta amor,
-                        </h1>
-                    </b-col>
-                    <b-col class="header-homePageBlue shadow" cols="5">
-                        <h1 class="title-headerPage mt-2 ps-0 text-start">
-                            adopta vida
-                        </h1>
-                    </b-col>
-                </b-row>
-                <b-row class="px-4 mt-2 justify-content-center">
-                    <b-col class="mx-5">
-                        <b-container>
-                            <CarouselGalery />
-                        </b-container>
-
-                    </b-col>
-                </b-row>
-                <b-row class="mt-3 px-5">
-                    <b-col class="" cols="11" sm="11" md="5" lg="5" xl="5">
-                        <Banner />
-                    </b-col>
-                    <b-col cols="11" sm="11" md="5" lg="5" xl="5">
-                        <b-carousel id="carousel-1" v-model="slide" :interval="4000" controls indicators>
-
-
-                            <b-carousel-slide class="friends" img-blank>
-                                <div class="friends border pt-3">
-                                    <b-row class="border mt-5">
-                                        <b-col class="pt-5" cols="112" sm="12" md="5" lg="5" xl="5">
-                                            <b-img class="mt-5" v-bind="mainProps" fluid src="https://picsum.photos/250/250/?image=54"
-                                                rounded alt="Rounded image"></b-img>
-                                        </b-col>
-                                        <b-col cols="12" sm="12 " md="5" lg="5" xl="5">
-                                            <b-row>
-                                    
-                                            </b-row>
-                                        </b-col>
-                                    </b-row>
-                                </div>
-                            </b-carousel-slide>
-                        </b-carousel>
-                    </b-col>
-                </b-row>
-                <b-row class=" my-3 justify-content-center mx-3">
-                    <b-col class="info-homepage" cols="12">
-                        <div class="title-headerPage mt-5 mb-3 text-center">
-                            <h4>Tu compañero ideal está esperando: descubre los amigos que tenemos para ti.</h4>
-                            <b-button class="home">Vamos a conocerlos</b-button>
-                        </div>
-                    </b-col>
-                </b-row>
+    <b-container fluid>
+        <b-row class="justify-content-center mt-3">
+            <b-col class="header-homePage shadow" cols="5" md="5" lg="5" xl="5">
+                <h1 class="title-headerPage mt-2 text-end">
+                    Adopta amor,
+                </h1>
+            </b-col>
+            <b-col class="header-homePageBlue shadow" cols="5" md="5" lg="5" xl="5">
+                <h1 class="title-headerPage mt-2 text-start">
+                    adopta vida
+                </h1>
             </b-col>
         </b-row>
+        <b-row class="px-4 mt-3 d-flex justify-content-center">
+            <b-col class="px-4" cols="12" sm="12" md="10" lg="10" xl="11">
+                <CarouselGalery />
+            </b-col>
+        </b-row>
+        <b-row class="mb px-0 justify-content-center">
+            <b-col class="ps-0" cols="11" sm="11" md="5" lg="5" xl="5">
+                <Banner class="w-100" />
+            </b-col>
+            <b-col class="my-0 px-0 " cols="11" sm="11" md="5" lg="5" xl="5">
+                <div class="friends mt-3 p-2 mb-0">
+                    <h4 class="mb-0">Conoce a nuestros amigos...</h4>
+                </div>
+                <b-carousel v-model="slide" img-width="1050" img-height="960" :interval="4000" controls>
+
+                    <b-carousel-slide img-blank class="d-flex justify-content-center align-items-center" caption-html=' <div>
+                            <b-row class="border d-flex justify-content-center align-items-center">
+                                <b-col class="border" cols="11" sm="11" md="5" lg="5" xl="5">
+                                    <img src="https://picsum.photos/250/250/?image=54"
+                                        style="max-width: 120rem; max-height: 80rem; object-fit: contain;" rounded></img>
+                                </b-col>
+                                <b-col class="border" cols="11" sm="11" md="5" lg="5" xl="5"></b-col>
+                            </b-row>
+                        </div>'>
+                    </b-carousel-slide>
+                </b-carousel>
+            </b-col>
+        </b-row>
+        <b-row class="justify-content-center mt-3">
+            <b-col class="px-1_5" cols="11" sm="11" md="11" lg="11" xl="10">
+                <div class="title-headerPage info-homepage w-100 mb-2 py-3  text-center w-100">
+                    <h4>Tu compañero ideal está esperando: descubre los amigos que tenemos para ti.</h4>
+                    <b-button class="home">Vamos a conocerlos</b-button>
+                </div>
+            </b-col>
+        </b-row>
+
 
     </b-container>
 </template>
