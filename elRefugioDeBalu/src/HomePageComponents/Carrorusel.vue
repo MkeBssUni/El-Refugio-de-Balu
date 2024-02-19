04316d63426a4ce47e1893605e0e69f79d9ac912
AlixStart313 <20213tn014@utez.edu.mx>
2024-02-18 18:04:15 -0600

    [MODIFY] landingPage y animalCategories page

--- elRefugioDeBalu/src/HomePageComponents/Carrorusel.vue
+++ elRefugioDeBalu/src/HomePageComponents/Carrorusel.vue
@@ -1,33 +1,33 @@
 <template>
     <div class="gallery mt-3">
         <figure class="carrusel ms-2 me-2">
-            <img src="../assets/imgs/Galery1.png" alt="Lakeview Elegance preview">
-            <figcaption class="carrusel py-5 me-2 text-center">
-                <h3 class="textGalery mx-5">Donde los corazones se encuentran: adopciones que transforman vidas.</h3>
+            <img src="../assets/imgs/carousel3.jpg" alt="Lakeview Elegance preview">
+            <figcaption class="carrusel py-4 px-5 me-2  text-center">
+                <h3 class="textGalery mt-0">Donde los corazones se encuentran: adopciones que transforman vidas.</h3>
             </figcaption>
         </figure>
         <figure class="carrusel mx-2">
-            <img src="../assets/imgs/Galery2.png" alt="Skyline Oasis preview">
-            <figcaption class="carrusel py-5 text-center">
-                <h3 class="textGalery">Encuentra tu compañero perfecto, adopta hoy. </h3>
+            <img src="../assets/imgs/carousel2.jpeg" alt="Skyline Oasis preview">
+            <figcaption class="carrusel py-4 mt-4  text-center">
+                <h3 class="textGalery mt-2  mx-5">El amor no tiene precio, ¡adopta y haz la diferencia!</h3>
             </figcaption>
         </figure>
         <figure class="carrusel mx-2">
-            <img src="../assets/imgs/Galery.png" alt="Vista Paradiso preview">
-            <figcaption class="carrusel py-5 text-center">
-                <h3 class="textGalery">El amor no tiene precio, ¡adopta y haz la diferencia!</h3>
+            <img src="../assets/imgs/carousel1.jpg" alt="Vista Paradiso preview">
+            <figcaption class="carrusel py-4 mt-4  text-center">
+                <h3 class="textGalery mt-3">Encuentra tu compañero perfecto, adopta hoy.</h3>
             </figcaption>
         </figure>
         <figure class="carrusel ms-2">
-            <img src="../assets/imgs/Galery4.png" alt="Villa Verde preview">
-            <figcaption class="carrusel py-5 text-center">
-                <h3 class="textGalery">Adopta, no compres: una decisión que salva vidas</h3>
+            <img src="../assets/imgs/carouse4.jpg" alt="Villa Verde preview">
+            <figcaption class="carrusel py-4 mt-4 text-center">
+                <h3 class="textGalery mt-3">Adopta, no compres: una decisión que salva vidas</h3>
             </figcaption>
         </figure>
         <figure class="carrusel ms-2 me-2">
-            <img src="../assets/imgs/Galery4.png" alt="Villa Verde preview">
-            <figcaption class="carrusel py-5 text-center">
-                <h3 class="textGalery">Adopta, no compres: una decisión que salva vidas</h3>
+            <img src="../assets/imgs/div2.jpeg" alt="Villa Verde preview">
+            <figcaption class="carrusel py-4 mt-4 text-center">
+                <h3 class="textGalery mt-3">Darle un hogar a quien lo necesita: tu oportunidad de cambiar el mundo</h3>
             </figcaption>
         </figure>
     </div>
