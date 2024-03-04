f1065633a25b94d043768583204492ffdf50cdca
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-04 16:58:43 -0600

    [ADD] initial config, insert roles and statusses

--- backend/src/main/java/com/balu/backend/modules/roles/model/Role.java
+++ backend/src/main/java/com/balu/backend/modules/roles/model/Role.java
@@ -19,7 +19,13 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(columnDefinition = "VARCHAR(40)", nullable = false, unique = true)
+    @Enumerated(EnumType.STRING)
     private Roles name;
     @OneToMany(mappedBy = "role")
     private Set<User> users;
+
+    public Role(Roles name) {
+        this.name = name;
+    }
 }
--- backend/src/main/java/com/balu/backend/modules/statusses/model/IStatusRepository.java
+++ backend/src/main/java/com/balu/backend/modules/statusses/model/IStatusRepository.java
@@ -3,6 +3,9 @@ package com.balu.backend.modules.statusses.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IStatusRepository extends JpaRepository<Status,Long> {
+    Optional<Status> findByName(Statusses name);
 }
