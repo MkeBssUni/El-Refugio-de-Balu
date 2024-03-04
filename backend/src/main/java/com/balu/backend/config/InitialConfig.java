49e9f902b1ae260e88b46d97bc5609a1bbac932f
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-04 16:02:51 -0600

    [ADD] status repository

---
+++ backend/src/main/java/com/balu/backend/config/InitialConfig.java
@@ -0,0 +1,9 @@
+package com.balu.backend.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InitialConfig implements CommandLineRunner {
+    private final 
+}
