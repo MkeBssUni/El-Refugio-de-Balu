501dd5d7008253fd8ee8140eaee00c769e260dc4
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-04 16:11:32 -0600

    [ADD] status repository

--- backend/src/main/java/com/balu/backend/modules/statusses/model/IStatusRepository.java
+++ backend/src/main/java/com/balu/backend/modules/statusses/model/IStatusRepository.java
@@ -1,2 +1,8 @@
-package com.balu.backend.modules.statusses.model;public interface IStatusRepository {
+package com.balu.backend.modules.statusses.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IStatusRepository extends JpaRepository<Status,Long> {
 }
