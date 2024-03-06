2bb5ece82f20f472272fdbc990ad8c32202e87df
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-06 08:14:16 -0600

    [ADD] validations

--- backend/src/main/java/com/balu/backend/kernel/ErrorMessages.java
+++ backend/src/main/java/com/balu/backend/kernel/ErrorMessages.java
@@ -2,5 +2,8 @@ package com.balu.backend.kernel;
 
 public enum ErrorMessages {
     DUPLICATE_RECORD,
-    ROLE_NOT_FOUND
+    ROLE_NOT_FOUND,
+    INVALID_FIELD,
+    INVALID_FORMAT,
+    MISSING_FIELDS,
 }
