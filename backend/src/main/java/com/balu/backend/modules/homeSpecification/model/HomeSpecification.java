212a7b6b697690e5d2102fc88b9a138dfde204d6
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-07 11:06:59 -0600

    [ADD] home specification and home images model

---
+++ backend/src/main/java/com/balu/backend/modules/homeSpecification/model/HomeSpecification.java
@@ -0,0 +1,33 @@
+package com.balu.backend.modules.homeSpecification.model;
+
+import com.balu.backend.modules.adresses.model.Address;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "home_specifications")
+public class HomeSpecification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "VARCHAR(40)")
+    @Enumerated(EnumType.STRING)
+    private HomeTypes type;
+    @Column(columnDefinition ="BOOLEAN")
+    private boolean outdoorArea;
+    private int numberOfResidents;
+    @OneToOne(mappedBy = "homeSpecification")
+    @JsonIgnore
+    private Address address;
+    @OneToOne(mappedBy = "homeSpecification")
+    @JsonIgnore
+    private HomeImage homeImage;
+}
---
+++ backend/src/main/java/com/balu/backend/modules/homeSpecification/model/HomeTypes.java
@@ -0,0 +1,15 @@
+package com.balu.backend.modules.homeSpecification.model;
+
+public enum HomeTypes {
+    DEPARTAMENTO,
+    CASA,
+    TERRENO,
+    BUNGALOW,
+    CONDOMINIO,
+    FRACCIONAMIENTO,
+    LOFT,
+    PENTHOUSE,
+    RANCHO,
+    QUINTA,
+    HACIENDA
+}
