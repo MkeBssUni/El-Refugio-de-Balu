d62b3aa9c778bff93cc7c28b4efea87d0690a128
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-07 14:34:57 -0600

    [UPDATE] added json include properties

--- backend/src/main/java/com/balu/backend/modules/adoptionRequests/model/AdoptionRequest.java
+++ backend/src/main/java/com/balu/backend/modules/adoptionRequests/model/AdoptionRequest.java
@@ -3,6 +3,7 @@ package com.balu.backend.modules.adoptionRequests.model;
 import com.balu.backend.modules.pets.model.Pet;
 import com.balu.backend.modules.statusses.model.Status;
 import com.balu.backend.modules.users.model.User;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,9 +24,11 @@ public class AdoptionRequest {
     private Long id;
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JsonIncludeProperties({"id", "username"})
     private User user;
     @ManyToOne
     @JoinColumn(name = "pet_id", referencedColumnName = "id")
+    @JsonIncludeProperties({"id", "username"})
     private Pet pet;
     @Column(columnDefinition = "json", nullable = false)
     private String images;
