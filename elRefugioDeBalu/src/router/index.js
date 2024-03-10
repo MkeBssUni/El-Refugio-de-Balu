0ec82ee71ea179a94fdb9c0bde54a94179316a7a
Andrea <20213tn008@utez.edu.mx>
2024-03-10 17:53:01 -0600

    [ADD] Interface SelfRegistration

--- elRefugioDeBalu/src/router/index.js
+++ elRefugioDeBalu/src/router/index.js
@@ -83,6 +83,11 @@ const routes = [
         path: "/UsersForm",
         name: "UsersForm",
         component: () => import("../modules/users/components/UsersForm.vue"),
+      },
+      {
+        path: "/SelfRegistration",
+        name: "SelfRegistration",
+        component: () => import("../modules/users/components/SelfRegistration.vue"),
       }
     ],
   },
