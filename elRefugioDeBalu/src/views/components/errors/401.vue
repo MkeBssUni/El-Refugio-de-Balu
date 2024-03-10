f53fd9dcf19b8698760390b40984ec668201955a
Cristina <20213tn151@utez.edu.mx>
2024-03-10 02:14:06 -0600

    [ADD] screen errors

---
+++ elRefugioDeBalu/src/views/components/errors/401.vue
@@ -0,0 +1,20 @@
+<template>
+   <img src="../../../assets/imgs/error/401.svg" class="error">
+</template>
+
+<script>
+export default {
+    name: '401'
+}
+</script>
+
+<style>
+.error {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  width: 100%;
+}
+
+</style>
---
+++ elRefugioDeBalu/src/views/components/errors/500.vue
@@ -0,0 +1,20 @@
+<template>
+   <img src="../../../assets/imgs/error/500.svg" class="error">
+</template>
+
+<script>
+export default {
+    name: '500'
+}
+</script>
+
+<style>
+.error {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  width: 100%;
+}
+
+</style>
