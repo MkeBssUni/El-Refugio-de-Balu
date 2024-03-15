1ca5202d876e3809d48c84bc17fc8eb6831fb4fd
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-15 09:58:37 -0600

    [UPDATE] get users paged

--- elRefugioDeBalu/src/modules/users/components/UsersList.vue
+++ elRefugioDeBalu/src/modules/users/components/UsersList.vue
@@ -1,79 +1,53 @@
 <template>
   <div class="">
-    <Encabezado
-      color="#00737e"
-      :imagenUrl="require('@/assets/imgs/perroblanco.png')"
-      titulo="Lista de Administradores y Moderadores"
-    />
+    <Encabezado color="#00737e" :imagenUrl="require('@/assets/imgs/perroblanco.png')"
+      titulo="Lista de Administradores y Moderadores" />
     <b-container fluid>
       <b-row class="justify-content-center px-4">
         <b-col cols="12" md="8" class="pt-0 pt-md-3 text-center">
           <b-input-group class="mt-3">
-            <b-form-input
-              type="text"
-              placeholder="Buscar..."
-              v-model="search"
-              class="form-control-md border-right-0"
-            ></b-form-input>
+            <b-form-input type="text" placeholder="Buscar..." v-model="searchValue" @keyup.enter="getUsersPaged"
+              class="form-control-md border-right-0"></b-form-input>
             <b-input-group-append>
-              <b-icon
-                icon="search"
-                style="
+              <b-icon icon="searchValue" style="
                   background-color: transparent;
                   position: absolute;
                   right: 10px;
                   top: 50%;
                   transform: translateY(-50%);
-                "
-              ></b-icon>
+                "></b-icon>
             </b-input-group-append>
           </b-input-group>
         </b-col>
         <b-col cols="12" md="4" class="pt-0 pt-md-3">
-          <b-button
-            variant="outline-success"
-            class="mt-3 d-flex align-items-center justify-content-center"
-            to='/usersForm'
-          >
+          <b-button variant="outline-success" class="mt-3 d-flex align-items-center justify-content-center"
+            to='/usersForm'>
             <b-icon icon="person-plus-fill" font-scale="1"></b-icon> Agregar
           </b-button>
         </b-col>
       </b-row>
       <b-row class="px-4 pt-4">
         <b-col cols="12">
-          <b-table
-            :fields="fields"
-            :items="filteredUsers"
-            :filter="search"
-            label-sort-asc=""
-            label-sort-desc=""
-            no-sort-reset
-            :per-page="perPage"
-            :current-page="currentPage"
-            responsive
-            small
-            striped
-            hover
-            class="text-center custom-scroll-style"
-          >
+          <b-table :fields="fields" :items="users" :filter="searchValue" label-sort-asc="" label-sort-desc=""
+            no-sort-reset :per-page="size" :current-page="page" responsive small striped hover
+            class="text-center custom-scroll-style">
             <template #cell(actions)="data">
-              <b-button
-                @click="showDeleteConfirmation(data.item)"
-                pill
-                size="sm"
-                variant="outline-danger"
-                class="px-2"
-              >
-              <b-icon icon="pencil-fill"></b-icon>
-              </b-button>
+              <div class="d-flex justify-content-center">
+                <b-button :variant="data.item.blocked ? 'outline-success' : 'outline-danger'"
+                  class="d-flex align-items-center">
+                  <b-icon :icon="data.item.blocked ? 'arrow-up-square' : 'arrow-down-square'"></b-icon>
+                </b-button>
+              </div>
             </template>
 
-            <template #cell(status)="data">
-              <b-badge v-if="data.value === 'Activo'" variant="success">
-                {{ data.value }}
+            <template #cell(blocked)="data">
+              <b-badge :variant="data.item.blocked ? 'danger' : 'success'">
+                {{ data.item.blocked ? "Bloqueado" : "Activo" }}
               </b-badge>
-              <b-badge v-else-if="data.value === 'Inactivo'" variant="danger">
-                {{ data.value }}
+            </template>
+            <template #cell(role_name)="data">
+              <b-badge :variant="data.item.role_name === 'ADMINISTRADOR' ? 'warning' : 'info'">
+                {{ data.item.role_name === 'ADMINISTRADOR' ? "Administrador" : "Moderador" }}
               </b-badge>
             </template>
           </b-table>
@@ -81,13 +55,14 @@
       </b-row>
       <b-row class="pt-4">
         <b-col cols="12">
-          <b-pagination
-            pills
-            v-model="currentPage"
-            :total-rows="filteredUsers.length"
-            :per-page="perPage"
-            align="center"
-          />
+          <b-row class="justify-content-center">
+            <b-col cols="6" md="4" class="pt-0 pt-md-3">
+              <b-form-select :options="options" v-model="size" class="form-select"
+                @change="getUsersPaged"></b-form-select>
+            </b-col>
+          </b-row>
+          <b-pagination pills v-model="page" :total-rows="users.length" :per-page="size" align="center">
+          </b-pagination>
         </b-col>
       </b-row>
     </b-container>
@@ -98,93 +73,42 @@
 import Swal from "sweetalert2";
 
 import Encabezado from "../../../views/components/Encabezado.vue";
+import gatoWalkingGif from "@/assets/imgs/gatoWalking.gif";
+import instance from "../../../config/axios";
+import { decrypt } from "../../../kernel/hashFunctions";
 export default {
   data() {
     return {
-      search: "",
-      perPage: 5,
-      currentPage: 1,
+      searchValue: "",
+      size: 10,
+      page: 1,
       fields: [
-        { key: "name", label: "Nombre" },
-        { key: "lastName", label: "Apellido Paterno" },
-        { key: "middleName", label: "Apellido Materno" },
-        { key: "email", label: "Correo Electrónico" },
-        { key: "phoneNumber", label: "Numero de teléfono" },
-        { key: "status", label: "Estado" },
+        { key: "fullname", label: "Nombre completo" },
+        { key: "username", label: "Correo Electrónico" },
+        { key: "phone", label: "Numero de teléfono" },
+        { key: "blocked", label: "Estado" },
+        { key: "role_name", label: "Rol" },
         { key: "actions", label: "Acciones" },
       ],
       users: [
         {
-          name: "Lisseth Georgina",
-          lastName: "Fuentes",
-          middleName: "Figueroa",
-          email: "liss@gmail.com",
-          phoneNumber: "7773778161",
-          status: "Activo",
-        },
-        {
-          name: "Anna christina",
-          lastName: "Bustos",
-          middleName: ".",
-          email: "Criss@gmail.com",
-          phoneNumber: "7771237683",
-          status: "Activo",
-        },
-        {
-          name: "José Miguel",
-          lastName: "Aguario",
-          middleName: "Díaz",
-          email: "Mike@gmail.com",
-          phoneNumber: "7778745609",
-          status: "Inactivo",
-        },
-        {
-          name: "Valeria Marianne ",
-          lastName: "Santos",
-          middleName: "Perez",
-          email: "Marianne@gmail.com",
-          phoneNumber: "7774567890",
-          status: "Activo",
-        },
+          id: null,
+          fullname: "",
+          username: "",
+          phone: "",
+          blocked: false,
+        }
       ],
+      pageResponse: {},
+      options: [1, 5, 10, 20, 50]
     };
   },
-  computed: {
-    filteredUsers() {
-      return this.users.filter((user) => {
-        const nameMatch = user.name
-          .toLowerCase()
-          .includes(this.search.toLowerCase());
-        const lastNameMatch = user.lastName
-          .toLowerCase()
-          .includes(this.search.toLowerCase());
-        const middleNameMatch = user.middleName
-          .toLowerCase()
-          .includes(this.search.toLowerCase());
-        const emailMatch = user.email
-          .toLowerCase()
-          .includes(this.search.toLowerCase());
-        const phoneNumberMatch = user.phoneNumber
-          .toLowerCase()
-          .includes(this.search.toLowerCase());
-        const statusMatch = user.status
-          .toLowerCase()
-          .includes(this.search.toLowerCase());
-
-        return (
-          nameMatch ||
-          lastNameMatch ||
-          middleNameMatch ||
-          emailMatch ||
-          phoneNumberMatch ||
-          statusMatch
-        );
-      });
-    },
+  mounted() {
+    this.getUsersPaged();
   },
   methods: {
-    editUser(user) {},
-    deleteUser(user) {},
+    editUser(user) { },
+    deleteUser(user) { },
     showDeleteConfirmation(user) {
       Swal.fire({
         title: "¿Estás seguro?",
@@ -201,6 +125,49 @@ export default {
         }
       });
     },
+    async getUsersPaged() {
+      try {
+        Swal.fire({
+          title: "Cargando...",
+          text: "Estamos cargando los usuarios, espera un momento",
+          imageUrl: gatoWalkingGif,
+          imageWidth: 160,
+          imageHeight: 160,
+          showConfirmButton: false,
+        })
+        const token = await decrypt(localStorage.getItem('token'))
+        const response = await instance.post(`/person/paged/?page=${this.page - 1}&?size=${this.size}`
+          , {
+            searchValue: this.searchValue,
+          }, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        console.log("se vuellve a cargar este bisne")
+        this.users = response.data.data.content
+        this.pageResponse = response.data.data
+
+        for await (const user of this.users) {
+          user.username = await decrypt(user.username)
+          user.phone = await decrypt(user.phone)
+        }
+
+        Swal.close()
+      } catch (error) {
+        Swal.fire({
+          title: "Error",
+          text: "Ocurrió un error al cargar los usuarios",
+          icon: "error",
+          iconColor: "#FF0000",
+          showConfirmButton: true,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.$router.push("/home");
+          }
+        });
+      }
+    }
   },
   components: {
     Encabezado,
@@ -212,6 +179,7 @@ export default {
 .form-select {
   width: 100%;
 }
+
 .font-size-lg {
   font-size: 1.5rem;
 }
