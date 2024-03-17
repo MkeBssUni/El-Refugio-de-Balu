4233b561f6be0bce2796dfd32778a34431b560a9
Cristina <20213tn151@utez.edu.mx>
2024-03-17 00:13:49 -0600

    [CREATE] viewAdoptionRequest by moderador

---
+++ elRefugioDeBalu/src/modules/adoptions/adapters/views/ViewAplicationRequest.vue
@@ -0,0 +1,316 @@
+<template>
+  <div>
+    <b-card bg-variant="light" class="mx-3 my-3 box-shadow-pretty">
+      <b-row>
+        <b-col cols="12" sm="12" lg="4" md="4">
+          <b-card
+            bg-variant="card-content-secondary-orange"
+            class="text-center box-shadow-pretty"
+          >
+            <b-card-text>Datos de solicitud</b-card-text>
+          </b-card>
+          <b-card
+            bg-variant="card-content-orange"
+            class="text-center my-1 box-shadow-pretty"
+          >
+            <b-card-text>
+              <b>Fecha de solicitud:</b>
+              25 de febrero del 2024
+            </b-card-text>
+          </b-card>
+          <b-card bg-variant="light" class="text-center my-1 box-shadow-pretty">
+            <b-card-text>
+              <b>Estado:</b>
+              <br />
+              <b-badge variant="warning">Pendiente</b-badge>
+              <br />
+              Debido a que esta en pendiente aun estamos revisando tu
+              información...
+            </b-card-text>
+          </b-card>
+        </b-col>
+        <b-col cols="12" sm="12" lg="8" md="8">
+          <b-row>
+            <b-col cols="12" lg="12" md="12">
+              <b-card-group deck>
+                <b-card
+                  bg-variant="orange"
+                  class="text-center box-shadow-pretty tam-enca"
+                >
+                <b-card-title>
+                    Datos personales
+                </b-card-title>
+                </b-card>
+                <b-card
+                  bg-variant="light"
+                  class="box-shadow-pretty text-center"
+                >
+                <b-row>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                    <b>Nombre:</b> Anna Christina Bustos
+                </b-card-text>
+
+              </b-col>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                  <b>Edad</b>
+                  22 años
+                </b-card-text>
+              </b-col>
+            </b-row>
+                </b-card>
+              </b-card-group>
+            </b-col>
+            <b-col cols="12" lg="12" md="12" class="my-3">
+              <b-card-group deck>
+                <b-card
+                  bg-variant="orange"
+                  class="text-center box-shadow-pretty tam-enca"
+                >
+                <b-card-title>
+                    Lugar que radica
+                </b-card-title>
+                </b-card>
+                <b-card
+                  bg-variant="light"
+                  class="box-shadow-pretty text-center"
+                >
+                <b-row>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                    <b>Municipio:</b> Cuernavaca
+                </b-card-text>
+
+              </b-col>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                  <b>Dirección:</b>
+                  Melchor Ocampo #203 Col. Empleaod 
+                </b-card-text>
+              </b-col>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                  <b>Estado:</b>
+                  Morelos
+                </b-card-text>
+              </b-col>
+              
+            </b-row>
+                </b-card>
+              </b-card-group>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="12" sm="12" lg="4" md="4">
+          <b-card bg-variant="light" class="box-shadow-pretty text-center">
+            <b-card-title>Lugar de residencia</b-card-title>
+            <b-card-body>
+              <b-card-content>
+                <b-card-text
+                  >Lugar en la que dormira o descansara la mascota</b-card-text
+                >
+                <img
+                  src="https://t2.ea.ltmcdn.com/es/posts/0/9/4/como_hacer_una_casa_para_perros_20490_orig.jpg"
+                  class="homePhotos"
+                />
+              </b-card-content>
+            </b-card-body>
+            <b-card-body>
+              <b-card-content>
+                <b-card-text
+                  >Lugar en el que vivira la mascota (casa)</b-card-text
+                >
+                <img
+                  src="https://t2.ea.ltmcdn.com/es/posts/0/9/4/como_hacer_una_casa_para_perros_20490_orig.jpg"
+                  class="homePhotos"
+                />
+              </b-card-content>
+            </b-card-body>
+            <b-card-body>
+              <b-card-content>
+                <b-card-text
+                  >Lugar en el que jugará la mascota (casa)</b-card-text
+                >
+                <img
+                  src="https://t2.ea.ltmcdn.com/es/posts/0/9/4/como_hacer_una_casa_para_perros_20490_orig.jpg"
+                  class="homePhotos"
+                />
+              </b-card-content>
+            </b-card-body>
+          </b-card>
+        </b-col>
+        <b-col cols="12" sm="12" lg="8" md="8">
+          <b-card bg-variant="light" class="my-3 box-shadow-pretty">
+            <b-card-title class="text-center">Motivos de adopción</b-card-title>
+            <b-row>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                  <b>¿Tienes o has tenido otros animales de compañia?</b>
+                  Si, una gatita antes pero murió por peleas callejeras
+                </b-card-text>
+              </b-col>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                  <b>¿En que parte de la casa lo planeas tener?</b>
+                  En el patio y en mi salita ahí se puede distraer
+                </b-card-text>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                  <b
+                    >¿Las personas con quien vives están de acuerdo en
+                    adoptar?</b
+                  >
+                  Si, claro ellas estan muy emocionadas en tener la gatita
+                </b-card-text>
+              </b-col>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                  <b>Comentarios adicionales</b>
+                  No se regitro un comentario adicional
+                </b-card-text>
+              </b-col>
+            </b-row>
+          </b-card>
+          <b-card bg-variant="light" class="my-3 box-shadow-pretty">
+            <b-card-title class="text-center"
+              >Experiencias previas</b-card-title
+            >
+            <b-row>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                  <b>¿Cuál fue tu ultima mascota?</b>
+                  Hace un año tenía una mascota era una gatita
+                </b-card-text>
+              </b-col>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                  <b>¿Cuando se enfermaba la mascota que hacias?</b>
+                  Llevarla a un veterinario
+                </b-card-text>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                  <b>¿Qué tipo de mascotas haz tenido antes?</b>
+                  Si, una gatita
+                </b-card-text>
+              </b-col>
+              <b-col cols="12" sm="12" lg="6" md="6" xl="6">
+                <b-card-text>
+                  <b>¿Qué recuerdos tienes con tu mascota?</b>
+                  No se regitro un recuerdo
+                </b-card-text>
+              </b-col>
+            </b-row>
+          </b-card>
+          <b-card bg-variant="light" class="my-3 box-shadow-pretty">
+            <b-card-title class="text-center"
+              >Información adicional</b-card-title
+            >
+            <b-row>
+              <b-col cols="12" sm="12" lg="12" md="12" xl="12">
+                <b-card-text>
+                  No se registraron comentarios adicionales
+                </b-card-text>
+              </b-col>
+            </b-row>
+          </b-card>
+        </b-col>
+      </b-row>
+      <div class="container-fluid d-flex justify-content-end">
+        <b-button
+          type="submit"
+          variant="outline-dark-secondary-blue pill"
+          class="mt-3 rounded-button mx-4"
+        >
+          Regresar
+        </b-button>
+        <b-button
+          type="submit"
+          variant="outline-danger pills"
+          class="mt-3 rounded-button"
+        >
+          Cancelar solicitud
+        </b-button>
+      </div>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ViewAplicationRequest",
+};
+</script>
+
+<style scoped>
+.target {
+  background-image: url("../../../../assets/imgs/pawBackground.jpg") !important;
+  background-size: cover;
+  background-position: center;
+  box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px,
+    rgba(17, 17, 26, 0.05) 0px 8px 32px;
+}
+
+.image-pet {
+  width: 100%;
+  height: 100%;
+  border-radius: 15px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+  transition: transform 0.3s ease; /* Agrega una transición suave para el efecto de zoom */
+}
+
+/* Cuando el mouse pasa sobre la imagen */
+.image-pet:hover {
+  transform: scale(1.1); /* Aumenta el tamaño de la imagen al 110% */
+}
+
+.information-pet {
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+  transition: transform 0.3s ease; /* Agrega una transición suave para el efecto de zoom */
+}
+
+.information-pet:hover {
+  transform: scale(1.1); /* Aumenta el tamaño de la imagen al 110% */
+}
+
+.box-shadow-pretty {
+  box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px,
+    rgba(17, 17, 26, 0.05) 0px 8px 32px;
+}
+
+.my-line {
+  border: none;
+  height: 2px;
+  background-color: black;
+}
+
+.homePhotos {
+  width: 80%;
+  height: 100%;
+  border-radius: 15px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+  transition: transform 0.3s ease; /* Agrega una transición suave para el efecto de zoom */
+}
+
+/* Cuando el mouse pasa sobre la imagen */
+.homePhotos:hover {
+  transform: scale(1.1); /* Aumenta el tamaño de la imagen al 110% */
+}
+
+.rounded-button {
+  border-radius: 20px;
+  width: 20%;
+}
+
+.tam-enca {
+    height: 50px;
+}
+</style>
