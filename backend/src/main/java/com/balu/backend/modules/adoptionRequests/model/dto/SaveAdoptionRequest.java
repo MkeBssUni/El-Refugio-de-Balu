b2235865890d325ea2223803625fea9bdb93b0f4
Cristina <20213tn151@utez.edu.mx>
2024-03-18 00:36:50 -0600

    [CREATE] structure

--- backend/src/main/java/com/balu/backend/modules/adoptionRequests/model/dto/SaveAdoptionRequest.java
+++ backend/src/main/java/com/balu/backend/modules/adoptionRequests/model/dto/SaveAdoptionRequest.java
@@ -1,20 +1,2 @@
-package com.balu.backend.modules.adoptionRequests.model.dto;
-
-
-import com.balu.backend.modules.pets.model.Pet;
-import com.balu.backend.modules.statusses.model.Status;
-import com.balu.backend.modules.users.model.User;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class SaveAdoptionRequest {
-    private User user;
-    private Pet pet;
-    private String images;
-    private Status status;
-    private String reasons_for_adoption;
-    private String previous_experience;
-    private String additional_information;
+package com.balu.backend.modules.adoptionRequests.model.dto;public class SaveAdoptionRequest {
 }
