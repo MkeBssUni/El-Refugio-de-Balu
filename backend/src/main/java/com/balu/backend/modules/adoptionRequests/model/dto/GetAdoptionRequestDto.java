5a7be6192985a31f24264d1069cc97480c01dfa7
Cristina <20213tn151@utez.edu.mx>
2024-03-18 22:46:36 -0600

    [CREATE] repository and service

--- backend/src/main/java/com/balu/backend/modules/adoptionRequests/model/dto/GetAdoptionRequestDto.java
+++ backend/src/main/java/com/balu/backend/modules/adoptionRequests/model/dto/GetAdoptionRequestDto.java
@@ -1,19 +1,26 @@
 package com.balu.backend.modules.adoptionRequests.model.dto;
 
-
 import com.balu.backend.modules.pets.model.Pet;
 import com.balu.backend.modules.statusses.model.Status;
 import com.balu.backend.modules.users.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
-public class SaveAdoptionRequest {
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAdoptionRequestDto {
+    private long id;
     private User user;
     private Pet pet;
     private String images;
     private Status status;
+    private LocalDateTime createdAt;
     private String reasons_for_adoption;
     private String previous_experience;
     private String additional_information;
