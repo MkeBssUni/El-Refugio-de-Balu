d5033fce3b2a01f7740d268dc4d1dfaa35e7f20d
Andrea <20213tn008@utez.edu.mx>
2024-03-18 19:36:24 -0600

    [ADD] Profile interface

--- elRefugioDeBalu/src/router/index.js
+++ elRefugioDeBalu/src/router/index.js
@@ -113,6 +113,11 @@ const routes = [
         path: "/selfRegistration",
         name: "selfRegistration",
         component: () => import("../modules/users/components/SelfRegistration.vue"),
+      },
+      {
+        path: "/Profile",
+        name: "Profile",
+        component: () => import("../modules/users/components/Profile.vue"),
       }
     ],
   },
