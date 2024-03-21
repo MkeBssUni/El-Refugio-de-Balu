b3f7cc70ad8c38f805d4035051291cac2838c0b9
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-21 10:24:10 -0600

    [UPDATE] secured routes

--- elRefugioDeBalu/src/router/index.js
+++ elRefugioDeBalu/src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import { decrypt } from "../kernel/hashFunctions";
 
 Vue.use(VueRouter);
-const DEFAULT_TITLE = "El Refugio de Balu"
+const DEFAULT_TITLE = "El Refugio de Balu";
+
+const rolesAvailable = {
+  general: "GENERAL",
+  mod: "MODERADOR",
+  admin: "ADMINISTRADOR",
+};
 
 const routes = [
   {
@@ -10,21 +17,25 @@ const routes = [
     redirect: "/home",
   },
   {
-    path: "/404",
+    path: "*",
     name: "404",
     component: () => import("../views/components/errors/404.vue"),
-  },{
+  },
+  {
     path: "/403",
     name: "403",
+    meta: { title: "Acceso denegado" },
     component: () => import("../views/components/errors/403.vue"),
   },
   {
     path: "/500",
     name: "500",
     component: () => import("../views/components/errors/500.vue"),
-  },{
+  },
+  {
     path: "/401",
     name: "401",
+    meta: { title: "No autorizado" },
     component: () => import("../views/components/errors/401.vue"),
   },
   {
@@ -43,90 +54,187 @@ const routes = [
       {
         path: "/pets",
         name: "pets",
-        component: () => import("../modules/pets/adapters/views/PetCatalog.vue"),
+        meta: { title: "Mascotas disponibles" },
+        component: () =>
+          import("../modules/pets/adapters/views/PetCatalog.vue"),
       },
       {
         path: "petDetails",
         name: "petDetails",
-        component: () => import("../modules/pets/adapters/views/PetDetails.vue"),
+        meta: { title: "Detalles de la mascota" },
+        component: () =>
+          import("../modules/pets/adapters/views/PetDetails.vue"),
       },
       {
         path: "/favorites",
         name: "favorites",
-        component: () => import("../modules/pets/adapters/views/FavoriteCatalog.vue"),
+        meta: {
+          title: "Mis mascotas favoritas",
+          requiresAuth: true,
+          role: rolesAvailable.general,
+        },
+        component: () =>
+          import("../modules/pets/adapters/views/FavoriteCatalog.vue"),
       },
       {
         path: "/myPets",
         name: "myPets",
-        component: () => import("../modules/pets/adapters/views/MyPetCatalog.vue"),
+        meta: {
+          title: "Mis publicaciones",
+          requiresAuth: true,
+          role: rolesAvailable.general,
+        },
+        component: () =>
+          import("../modules/pets/adapters/views/MyPetCatalog.vue"),
       },
       {
         path: "/formAdoption",
         name: "formAdoption",
-        component: () => import("../modules/adoptions/adapters/views/FormAdoption.vue"),
-      },{
+        meta: {
+          title: "Solicitud de adopción",
+          requiresAuth: true,
+          role: rolesAvailable.general,
+        },
+        component: () =>
+          import("../modules/adoptions/adapters/views/FormAdoption.vue"),
+      },
+      {
         path: "/myAplicationAdoption",
         name: "myAplicationAdoption",
-        component: () => import("../modules/adoptions/adapters/views/MyAplicationAdoption.vue"),
-      },{
+        meta: {
+          title: "Mis Solicitud de adopción",
+          requiresAuth: true,
+          role: [rolesAvailable.general],
+        },
+        component: () =>
+          import(
+            "../modules/adoptions/adapters/views/MyAplicationAdoption.vue"
+          ),
+      },
+      {
         path: "/adoptionList",
         name: "adoptionList",
-        component: () => import("../modules/adoptions/adapters/views/AdoptionList.vue"),
-      },{
+        meta: {
+          title: "Solicitudes de adopción",
+          requiresAuth: true,
+          role: rolesAvailable.mod,
+        },
+        component: () =>
+          import("../modules/adoptions/adapters/views/AdoptionList.vue"),
+      },
+      {
         path: "/viewAplicationAdoptionRequest",
         name: "viewAplicationAdoptionRequest",
-        component: () => import("../modules/adoptions/adapters/views/ViewAplication.vue"),
+        meta: {
+          title: "Detalles de solicitud de adopción",
+          requiresAuth: true,
+          role: [rolesAvailable.general, rolesAvailable.mod],
+        },
+        component: () =>
+          import("../modules/adoptions/adapters/views/ViewAplication.vue"),
       },
       {
         path: "/login",
         name: "login",
+        meta: {
+          title: "Iniciar sesión",
+        },
         component: () => import("../modules/auth/adapters/views/Login.vue"),
       },
       {
         path: "/petList",
         name: "petList",
+        meta: {
+          title: "Mascotas asignadas",
+          requiresAuth: true,
+          role: rolesAvailable.mod,
+        },
         component: () => import("../modules/pets/adapters/views/PetList.vue"),
       },
       {
         path: "/petForm",
         name: "petForm",
-        component: () => import("../modules/pets/adapters/views/PetRegisterForm.vue"),
-      },      
+        meta: {
+          title: "Registrar mascota",
+          requiresAuth: true,
+          role: rolesAvailable.general,
+        },
+        component: () =>
+          import("../modules/pets/adapters/views/PetRegisterForm.vue"),
+      },
       {
         path: "/usersList",
         name: "usersList",
+        meta: {
+          title: "Usuarios",
+          requiresAuth: true,
+          role: rolesAvailable.admin,
+        },
         component: () => import("../modules/users/components/UsersList.vue"),
       },
       {
         path: "/animalCategories",
         name: "animals",
-        component: () => import("../modules/animals/adapters/views/AnimalCategories.vue"),
+        meta: {
+          title: "Categorías de animales",
+          requiresAuth: true,
+          role: rolesAvailable.admin,
+        },
+        component: () =>
+          import("../modules/animals/adapters/views/AnimalCategories.vue"),
       },
       {
         path: "/usersForm",
         name: "usersForm",
-        meta: { title: 'Registrar usuario'},
+        meta: {
+          title: "Registrar usuario",
+          requiresAuth: true,
+          role: rolesAvailable.admin,
+        },
         component: () => import("../modules/users/components/UsersForm.vue"),
       },
       {
         path: "/selfRegistration",
         name: "selfRegistration",
-        component: () => import("../modules/users/components/SelfRegistration.vue"),
+        meta:{
+          title: "Crear cuenta"
+        },
+        component: () =>
+          import("../modules/users/components/SelfRegistration.vue"),
       },
       {
         path: "/profile",
         name: "profile",
+        meta:{
+          title: "Perfil",
+          requiresAuth: true,
+          role: [rolesAvailable.general, rolesAvailable.mod, rolesAvailable.admin],
+        },
         component: () => import("../modules/users/components/Profile.vue"),
-      }
+      },
     ],
   },
 ];
 
 const router = new VueRouter({ routes });
 
-router.afterEach((to,from)=>{
-  Vue.nextTick(()=>{
-    document.title = to.meta.title || DEFAULT_TITLE
-  })
+router.beforeEach(async (to,from, next)=>{
+  
+  if(to.meta.requiresAuth){
+    let role = await decrypt(localStorage.getItem("role"));
+    if(role){
+      to.meta.role.includes(role) ? next() : next({name: "403"});
+    }else{
+      next({name: "401"});
+    }
+  }else{
+    next();
+  }
 })
+
+router.afterEach((to, from) => {
+  Vue.nextTick(() => {
+    document.title = to.meta.title || DEFAULT_TITLE;
+  });
+});
 export default router;
