767248093a29c0ac181e19c2a40f05cb4bcd04a1
Marianne <20213tn140@utez.edu.mx>
2024-03-25 08:32:36 -0600

    [FIX] fix enum names

--- backend/src/main/java/com/balu/backend/modules/statusses/model/Statusses.java
+++ backend/src/main/java/com/balu/backend/modules/statusses/model/Statusses.java
@@ -1,9 +1,9 @@
 package com.balu.backend.modules.statusses.model;
 
 public enum Statusses {
-    PENDIENTE,
-    CERRADA,
-    ADOPTADA,
-    EN_REVISION,
-    APROBADA,
+    PENDING,
+    CLOSED,
+    ADOPTED,
+    IN_REVISION,
+    APRROVED,
 }
