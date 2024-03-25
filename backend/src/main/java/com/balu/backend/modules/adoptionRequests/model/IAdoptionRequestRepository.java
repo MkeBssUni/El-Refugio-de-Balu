7f403a28f3774c9c6171940ea66b207191b1b7c9
Cristina <20213tn151@utez.edu.mx>
2024-03-25 14:59:24 -0600

    [UPDATE] news fields

--- backend/src/main/java/com/balu/backend/modules/adoptionRequests/model/IAdoptionRequestRepository.java
+++ backend/src/main/java/com/balu/backend/modules/adoptionRequests/model/IAdoptionRequestRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface IAdoptionRequestRepository  extends JpaRepository<AdoptionRequest,Long> {
     
 
+    Page<GetAdoptionRequestDto> findByUser_Id(int id);
+
 }
