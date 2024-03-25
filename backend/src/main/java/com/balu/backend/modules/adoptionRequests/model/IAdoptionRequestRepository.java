291bb87ea22018e63191ca9c98c86532494c0787
Cristina <20213tn151@utez.edu.mx>
2024-03-25 12:40:04 -0600

    [UPDATE] reabaseb back

--- backend/src/main/java/com/balu/backend/modules/adoptionRequests/model/IAdoptionRequestRepository.java
+++ backend/src/main/java/com/balu/backend/modules/adoptionRequests/model/IAdoptionRequestRepository.java
@@ -5,13 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IAdoptionRequestRepository  extends JpaRepository<AdoptionRequest,Long> {
-
-    // Para traer informacion por medio de un usuario
-    //listado de solicitudes
-    Page<AdoptionRequest> findAllByUser (String User);
-
-    // Para traer informacion por medio de un moderador
-    //listado de solicitudes
-
+    
 
 }
