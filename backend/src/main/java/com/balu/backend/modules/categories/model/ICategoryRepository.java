48c8f88d9d76ff5d49f4495f1e8dfc6bb5014641
Lisseth Fuentes <20211tn014@utez.edu.mx>
2024-03-26 15:11:20 -0600

    [FIX] modification of the query for the pager and a spelling mistake in the pager code

--- backend/src/main/java/com/balu/backend/modules/categories/model/ICategoryRepository.java
+++ backend/src/main/java/com/balu/backend/modules/categories/model/ICategoryRepository.java
@@ -1,7 +1,5 @@
 package com.balu.backend.modules.categories.model;
 
-import com.balu.backend.modules.categories.model.dto.GetCategoryListDto;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,11 +7,13 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
-public interface ICategoryRepository  extends JpaRepository<Category,Long> {
+public interface ICategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByStatusTrue();
+
     Category findByName(String name);
 
     @Modifying
@@ -26,11 +26,13 @@ public interface ICategoryRepository  extends JpaRepository<Category,Long> {
 
     @Query(value =
             "SELECT id, \n" +
-            "       name AS categoryName, \n" +
-            "       description AS categoryDescription, \n" +
-            "       image AS categoryImage, \n" +
-            "       status AS categoryStatus \n" +
-            "FROM categories \n" +
-            "WHERE LOWER(name) LIKE '%:name%' OR LOWER(description) LIKE '%:description%';",nativeQuery=true)
-    Page<ICategoryViewPaged> findAllPaged(String name, String description, Pageable peageable);
+                    "       name AS categoryName, \n" +
+                    "       description AS categoryDescription, \n" +
+                    "       image AS categoryImage, \n" +
+                    "       status AS categoryStatus \n" +
+                    "FROM categories \n" +
+                    "WHERE LOWER(name) LIKE %:name% OR LOWER(description) LIKE %:description%\n" +
+                    "ORDER BY id ASC",
+            nativeQuery = true)
+    Page<ICategoryViewPaged> findAllPaged(@Param("name") String name, @Param("description") String description, Pageable pageable);
 }
