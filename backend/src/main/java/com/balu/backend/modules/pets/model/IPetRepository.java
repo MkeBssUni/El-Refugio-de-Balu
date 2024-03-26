7ab9b3853b4762401caa5a726cf4233e5e91b31d
Marianne <20213tn140@utez.edu.mx>
2024-03-26 08:25:27 -0600

    [ADD] get pet catalog paged part 1

--- backend/src/main/java/com/balu/backend/modules/pets/model/IPetRepository.java
+++ backend/src/main/java/com/balu/backend/modules/pets/model/IPetRepository.java
@@ -1,9 +1,14 @@
 package com.balu.backend.modules.pets.model;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IPetRepository extends JpaRepository<Pet,Long> {
-
+    @Query(value = "", nativeQuery = true)
+    List<Pet> findAllPaged(Long categoryId, String size, String lifeStage, String gender, String location, Pageable pageable);
 }
