228b1bd697a169b1ae4a092d4663c8890e19a1a4
Marianne <20213tn140@utez.edu.mx>
2024-03-29 09:29:27 -0600

    [ADD] get my pets query

---
+++ backend/src/main/java/com/balu/backend/modules/pets/model/views/IMyPetsView.java
@@ -0,0 +1,9 @@
+package com.balu.backend.modules.pets.model.views;
+
+public interface IMyPetsView {
+    Long getId();
+    int getComments();
+    String getName();
+    String getLocation();
+    String getStatus();
+}
