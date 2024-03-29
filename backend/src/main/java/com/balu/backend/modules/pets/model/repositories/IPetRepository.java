409e20c82d1335a6277308bfdc58c266e5dd6aad
Marianne <20213tn140@utez.edu.mx>
2024-03-29 09:29:27 -0600

    [ADD] get my pets query

--- backend/src/main/java/com/balu/backend/modules/pets/model/repositories/IPetRepository.java
+++ backend/src/main/java/com/balu/backend/modules/pets/model/repositories/IPetRepository.java
@@ -2,6 +2,7 @@ package com.balu.backend.modules.pets.model.repositories;
 
 import com.balu.backend.modules.pets.model.Pet;
 import com.balu.backend.modules.pets.model.views.IMyPetsAsModView;
+import com.balu.backend.modules.pets.model.views.IMyPetsView;
 import com.balu.backend.modules.pets.model.views.IPetCredentialView;
 import com.balu.backend.modules.pets.model.views.IPetRequestsView;
 import com.balu.backend.modules.users.model.User;
@@ -45,6 +46,14 @@ public interface IPetRepository extends JpaRepository<Pet,Long> {
             "where p.user_moderator_id = ?1 and p.category_id = ?2 and lower(s.name) like %?3% and lower(p.name) like %?4% " +
             "group by p.id", nativeQuery = true)
     Page<IMyPetsAsModView> findMyPetsAsModByCategory(Long userId, Long categoryId, String status, String searchValue, Pageable pageable);
+
+    @Query(value = "select p.id as id, count(pc.id) as comments, p.name as name, concat(a.city, ', ', a.state) as location, lower(s.name) as status from pets p " +
+                        "inner join addresses a on p.user_owner_id = a.user_id " +
+                        "inner join statusses s on p.status_id = s.id " +
+                        "left join pet_comments pc on p.id = pc.pet_id " +
+                        "where p.user_owner_id = ?1 and lower(s.name) like %?2% and (lower(p.name) like %?3% or lower(a.city) like %?3% or lower(a.state) like %?3%) " +
+                        "group by p.id", nativeQuery = true)
+    Page<IMyPetsView> findMyPetsByOwner(Long ownerId, String status, String searchValue, Pageable pageable);
 
     boolean existsByIdAndOwner(Long id, User owner);
 
