fb13973d907f2df105e5e63948653f64c94369ef
Marianne <20213tn140@utez.edu.mx>
2024-03-30 00:30:58 -0600

    [ADD] header

--- elRefugioDeBalu/src/modules/pets/adapters/views/ModeratedPetList.vue
+++ elRefugioDeBalu/src/modules/pets/adapters/views/ModeratedPetList.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        <h1>Moderated Pet List</h1>
-    </div>
+    <b-container fluid>
+        <b-row>
+            <Encabezado color="#ffc876" :imagenUrl="require('@/assets/imgs/dog.png')"
+                titulo="Lista de seguimiento de mascotas" />
+        </b-row>
+    </b-container>
 </template>
 
 <script>
+import Encabezado from "../../../../views/components/Encabezado.vue";
 export default {
-
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+        
+    },
+    methods: {
+        
+    },
+    components: {
+        Encabezado
+    },
 }
 </script>
 
-<style>
-
-</style>
+<style scoped></style>
