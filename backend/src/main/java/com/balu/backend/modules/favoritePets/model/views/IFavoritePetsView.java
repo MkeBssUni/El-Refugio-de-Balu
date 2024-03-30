9ec1420c024daa11fdfdd89936ac26b9934ad0e8
Marianne <20213tn140@utez.edu.mx>
2024-03-30 17:36:28 -0600

    [UPDATE] return pet id when consulting my favorite pets

--- backend/src/main/java/com/balu/backend/modules/favoritePets/model/views/IFavoritePetsView.java
+++ backend/src/main/java/com/balu/backend/modules/favoritePets/model/views/IFavoritePetsView.java
@@ -2,6 +2,7 @@ package com.balu.backend.modules.favoritePets.model.views;
 
 public interface IFavoritePetsView {
     Long getId();
+    Long getPetId();
     String getName();
     String getImage();
     String getLocation();
