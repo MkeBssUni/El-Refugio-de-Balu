88f55849b37f85c951b58a6ac99ccbd9fc13233b
Marianne <20213tn140@utez.edu.mx>
2024-04-01 14:02:02 -0600

    [ADD] request changes consume part 1

--- elRefugioDeBalu/src/modules/pets/adapters/views/NewPetDetails.vue
+++ elRefugioDeBalu/src/modules/pets/adapters/views/NewPetDetails.vue
@@ -1,80 +1,83 @@
 <template>
-    <b-container fluid>
-        <b-row>
-            <b-col cols="12" class="mt-5 px-5">
-                <!-- md, lg, xl card -->
-                <b-card class="full-height-card p-4 d-none d-md-block card-shadow" bg-variant="gray">
-                    <LargeContent :pet="pet" />
-                    <hr class="divider my-0">
-                    <b-row class="mt-4 d-flex justify-content-end">
-                        <b-col cols="6" xl="3">
-                            <b-button variant="outline-success" @click="selectPet()"
-                                class="me-3 d-flex align-items-center justify-content-between w-100">
-                                <span class="me-2">Dar seguimiento</span>
-                                <b-icon icon="check-circle" font-scale="1.3"></b-icon>
-                            </b-button>
-                        </b-col>
-                        <b-col cols="6" xl="3">
-                            <b-button variant="outline-dark-orange"
-                                class="me-3 d-flex align-items-center justify-content-between w-100">
-                                <span class="me-2">Solicitar cambios</span>
-                                <b-icon icon="pencil" font-scale="1.3"></b-icon>
-                            </b-button>
-                        </b-col>
-                        <b-col cols="6" xl="3" class="mt-3 mt-xl-0">
-                            <b-button variant="outline-danger" @click="closePet()"
-                                class="me-3 d-flex align-items-center justify-content-between w-100">
-                                <span class="me-2">Finalizar</span>
-                                <b-icon icon="x-circle" font-scale="1.3"></b-icon>
-                            </b-button>
-                        </b-col>
-                        <b-col cols="6" xl="3" class="mt-3 mt-xl-0">
-                            <b-button variant="outline-secondary-gray" @click="goBack"
-                                class="d-flex align-items-center justify-content-between w-100">
-                                <span class="me-2">Regresar</span>
-                                <b-icon icon="arrow-left-circle" font-scale="1.3"></b-icon>
-                            </b-button>
-                        </b-col>
-                    </b-row>
-                </b-card>
-                <!-- xs, sm card -->
-                <b-card class="full-height-card p-4 d-block d-md-none card-shadow" bg-variant="gray">
-                    <SmallContent :pet="pet" />
-                    <hr class="divider my-0">
-                    <b-row class="mt-4 d-flex justify-content-end">
-                        <b-col cols="12" sm="6">
-                            <b-button variant="outline-success" @click="selectPet()"
-                                class="me-3 d-flex align-items-center justify-content-between w-100">
-                                <span class="me-2">Dar seguimiento</span>
-                                <b-icon icon="check-circle" font-scale="1.3"></b-icon>
-                            </b-button>
-                        </b-col>
-                        <b-col cols="12" sm="6" class="mt-3 mt-sm-0">
-                            <b-button variant="outline-dark-orange"
-                                class="me-3 d-flex align-items-center justify-content-between w-100">
-                                <span class="me-2">Solicitar cambios</span>
-                                <b-icon icon="pencil" font-scale="1.3"></b-icon>
-                            </b-button>
-                        </b-col>
-                        <b-col cols="12" sm="6" class="mt-3">
-                            <b-button variant="outline-danger" @click="closePet()"
-                                class="me-3 d-flex align-items-center justify-content-between w-100">
-                                <span class="me-2">Finalizar</span>
-                                <b-icon icon="x-circle" font-scale="1.3"></b-icon>
-                            </b-button>
-                        </b-col>
-                        <b-col cols="12" sm="6" class="mt-3">
-                            <b-button variant="outline-secondary-gray" @click="goBack"
-                                class="d-flex align-items-center justify-content-between w-100">
-                                <span class="me-2">Regresar</span>
-                                <b-icon icon="arrow-left-circle" font-scale="1.3"></b-icon>
-                            </b-button>
-                        </b-col>
-                    </b-row>
-                </b-card>
-            </b-col>
-        </b-row>
-    </b-container>
+    <div>
+        <b-container fluid>
+            <b-row>
+                <b-col cols="12" class="mt-5 px-5">
+                    <!-- md, lg, xl card -->
+                    <b-card class="full-height-card p-4 d-none d-md-block card-shadow" bg-variant="gray">
+                        <LargeContent :pet="pet" />
+                        <hr class="divider my-0">
+                        <b-row class="mt-4 d-flex justify-content-end">
+                            <b-col cols="6" xl="3">
+                                <b-button variant="outline-success" @click="selectPet()"
+                                    class="me-3 d-flex align-items-center justify-content-between w-100">
+                                    <span class="me-2">Dar seguimiento</span>
+                                    <b-icon icon="check-circle" font-scale="1.3"></b-icon>
+                                </b-button>
+                            </b-col>
+                            <b-col cols="6" xl="3">
+                                <b-button variant="outline-dark-orange" @click="showChangesModal()"
+                                    class="me-3 d-flex align-items-center justify-content-between w-100">
+                                    <span class="me-2">Solicitar cambios</span>
+                                    <b-icon icon="pencil" font-scale="1.3"></b-icon>
+                                </b-button>
+                            </b-col>
+                            <b-col cols="6" xl="3" class="mt-3 mt-xl-0">
+                                <b-button variant="outline-danger" @click="closePet()"
+                                    class="me-3 d-flex align-items-center justify-content-between w-100">
+                                    <span class="me-2">Finalizar</span>
+                                    <b-icon icon="x-circle" font-scale="1.3"></b-icon>
+                                </b-button>
+                            </b-col>
+                            <b-col cols="6" xl="3" class="mt-3 mt-xl-0">
+                                <b-button variant="outline-secondary-gray" @click="goBack"
+                                    class="d-flex align-items-center justify-content-between w-100">
+                                    <span class="me-2">Regresar</span>
+                                    <b-icon icon="arrow-left-circle" font-scale="1.3"></b-icon>
+                                </b-button>
+                            </b-col>
+                        </b-row>
+                    </b-card>
+                    <!-- xs, sm card -->
+                    <b-card class="full-height-card p-4 d-block d-md-none card-shadow" bg-variant="gray">
+                        <SmallContent :pet="pet" />
+                        <hr class="divider my-0">
+                        <b-row class="mt-4 d-flex justify-content-end">
+                            <b-col cols="12" sm="6">
+                                <b-button variant="outline-success" @click="selectPet()"
+                                    class="me-3 d-flex align-items-center justify-content-between w-100">
+                                    <span class="me-2">Dar seguimiento</span>
+                                    <b-icon icon="check-circle" font-scale="1.3"></b-icon>
+                                </b-button>
+                            </b-col>
+                            <b-col cols="12" sm="6" class="mt-3 mt-sm-0">
+                                <b-button variant="outline-dark-orange"
+                                    class="me-3 d-flex align-items-center justify-content-between w-100">
+                                    <span class="me-2">Solicitar cambios</span>
+                                    <b-icon icon="pencil" font-scale="1.3"></b-icon>
+                                </b-button>
+                            </b-col>
+                            <b-col cols="12" sm="6" class="mt-3">
+                                <b-button variant="outline-danger" @click="closePet()"
+                                    class="me-3 d-flex align-items-center justify-content-between w-100">
+                                    <span class="me-2">Finalizar</span>
+                                    <b-icon icon="x-circle" font-scale="1.3"></b-icon>
+                                </b-button>
+                            </b-col>
+                            <b-col cols="12" sm="6" class="mt-3">
+                                <b-button variant="outline-secondary-gray" @click="goBack"
+                                    class="d-flex align-items-center justify-content-between w-100">
+                                    <span class="me-2">Regresar</span>
+                                    <b-icon icon="arrow-left-circle" font-scale="1.3"></b-icon>
+                                </b-button>
+                            </b-col>
+                        </b-row>
+                    </b-card>
+                </b-col>
+            </b-row>
+        </b-container>
+        <ChangesModal :petId="petId" />
+    </div>
 </template>
 
 <script>
@@ -83,6 +86,7 @@ import instance from "../../../../config/axios";
 
 import SmallContent from "../components/PetSmallCardContent.vue"
 import LargeContent from "../components/PetLargeCardContent.vue"
+import ChangesModal from "../components/ChangesRequest.vue"
 import gatoWalkingGif from "@/assets/imgs/gatoWalking.gif";
 import { sizes, lifeStages, weightUnits, ageUnits } from "../../../../kernel/data/mappingDictionaries";
 
@@ -96,6 +100,7 @@ export default {
     data() {
         return {
             pet: {},
+            comment: ""
         }
     },
     methods: {
@@ -162,7 +167,7 @@ export default {
                     imageHeight: 160,
                     showConfirmButton: false
                 })
-                await instance.post(`/pet/select`, { 
+                await instance.post(`/pet/select`, {
                     pet: this.petId,
                     user: localStorage.getItem('userId'),
                     status: 'approved'
@@ -178,7 +183,7 @@ export default {
                 }).then(() => {
                     this.$router.push('/moderated/petList')
                 })
-            } catch (error) {                
+            } catch (error) {
                 Swal.fire({
                     title: 'Error',
                     text: 'Ocurrió un error al asignar la mascota',
@@ -189,9 +194,9 @@ export default {
                     showConfirmButton: false
                 }).then(() => {
                     this.$router.push('/petList')
-                })            
+                })
             }
-        },   
+        },
         async closePet() {
             try {
                 Swal.fire({
@@ -231,7 +236,10 @@ export default {
                     this.$router.push('/petList')
                 })
             }
-        }     
+        },
+        showChangesModal() {
+            this.$bvModal.show('changesModal');
+        },        
     },
     mounted() {
         if (!this.petId && localStorage.getItem('petId')) this.petId = localStorage.getItem('petId');
@@ -239,7 +247,8 @@ export default {
     },
     components: {
         SmallContent,
-        LargeContent
+        LargeContent,
+        ChangesModal
     }
 }
 </script>
