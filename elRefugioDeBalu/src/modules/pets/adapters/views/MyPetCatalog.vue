5656a4e81c215f149d153722e23dbe70f5a1ad61
Marianne <20213tn140@utez.edu.mx>
2024-04-01 02:10:45 -0600

    [ADD] get owned pets comments and add comment

--- elRefugioDeBalu/src/modules/pets/adapters/views/MyPetCatalog.vue
+++ elRefugioDeBalu/src/modules/pets/adapters/views/MyPetCatalog.vue
@@ -1,79 +1,85 @@
 <template>
-    <b-container fluid class="px-5">
-        <b-row class="mt-5 d-flex align-items-center">
-            <b-col cols="12" md="6" lg="5"
-                class="d-flex align-items-center justify-content-center justify-content-md-start">
-                <span class="custom-h1">Mis publicaciones de mascotas</span>
-            </b-col>
-            <b-col cols="12" md="6" lg="7" class="mt-3 mt-md-0">
-                <b-row class="d-flex justify-content-center">
-                    <b-col cols="12" sm="6" md="12" lg="5">
-                        <b-input-group>
-                            <b-form-input type="text" placeholder="Buscar..." id="searchValue"
-                                v-model.trim="form.searchValue" @keyup.enter="getMyPets()"></b-form-input>
-                            <b-button variant="dark-gray" type="button" id="searchValue" @click="getMyPets()"
+    <div>
+        <b-container fluid class="px-5">
+            <b-row class="mt-5 d-flex align-items-center">
+                <b-col cols="12" md="6" lg="5"
+                    class="d-flex align-items-center justify-content-center justify-content-md-start">
+                    <span class="custom-h1">Mis publicaciones de mascotas</span>
+                </b-col>
+                <b-col cols="12" md="6" lg="7" class="mt-3 mt-md-0">
+                    <b-row class="d-flex justify-content-center">
+                        <b-col cols="12" sm="6" md="12" lg="5">
+                            <b-input-group>
+                                <b-form-input type="text" placeholder="Buscar..." id="searchValue"
+                                    v-model.trim="form.searchValue" @keyup.enter="getMyPets()"></b-form-input>
+                                <b-button variant="dark-gray" type="button" id="searchValue" @click="getMyPets()"
+                                    class="d-flex align-items-center justify-content-between">
+                                    <b-icon icon="search"></b-icon>
+                                </b-button>
+                            </b-input-group>
+                        </b-col>
+                        <b-col cols="12" sm="6" md="12" lg="4" class="mt-3 mt-sm-0 mt-md-3 mt-lg-0">
+                            <b-input-group>
+                                <b-form-select v-model="form.status" class="form-select" @change="getMyPets()">
+                                    <option value="">Todos los estados</option>
+                                    <option v-for="status in statusses" :key="status.value" :value="status.value">
+                                        {{ status.text }}
+                                    </option>
+                                </b-form-select>
+                            </b-input-group>
+                        </b-col>
+                        <b-col cols="12" sm="6" md="12" lg="3" class="d-flex justify-content-center mt-3 mt-lg-0">
+                            <b-button variant="outline-success w-100" to="/petForm"
                                 class="d-flex align-items-center justify-content-between">
-                                <b-icon icon="search"></b-icon>
-                            </b-button>
-                        </b-input-group>
-                    </b-col>
-                    <b-col cols="12" sm="6" md="12" lg="4" class="mt-3 mt-sm-0 mt-md-3 mt-lg-0">
-                        <b-input-group>
-                            <b-form-select v-model="form.status" class="form-select" @change="getMyPets()">
-                                <option value="">Todos los estados</option>
-                                <option v-for="status in statusses" :key="status.value" :value="status.value">
-                                    {{ status.text }}
-                                </option>
-                            </b-form-select>
-                        </b-input-group>
-                    </b-col>
-                    <b-col cols="12" sm="6" md="12" lg="3" class="d-flex justify-content-center mt-3 mt-lg-0">
-                        <b-button variant="outline-success w-100" to="/petForm"
-                            class="d-flex align-items-center justify-content-between">
-                            <span class="mr-2">Agregar</span>
-                            <b-icon icon="plus-circle" class="ms-2"></b-icon>
-                        </b-button>
-                    </b-col>
-                </b-row>
-            </b-col>
-        </b-row>
-        <hr class="mt-4">
-        <b-row class="mb-5">
-            <b-col v-for="pet in pets" :key="pet.id" cols="12" sm="6" lg="4" xl="3" class="mt-3">
-                <b-card :key="pet.id" :img-src="pet.image" class="pet-card" no-body>
-                    <b-row v-if="pet.comments > 0" class="transparent absolute-position">
-                        <b-col cols="12" class="d-flex justify-content-end">
-                            <b-button class="py-2 d-flex align-items-center relative-position custom-transparent-button"
-                                pill v-b-tooltip.hover.left="'Ver comentarios'">
-                                <b-icon icon="chat-left-fill" font-scale="3"
-                                    class="text-dark-secondary-orange chat-icon mt-2"></b-icon>
-                                <span class="text-light comments">
-                                    {{ (pet.comments > 9) ? '9+' : pet.comments }}
-                                </span>
+                                <span class="mr-2">Agregar</span>
+                                <b-icon icon="plus-circle" class="ms-2"></b-icon>
                             </b-button>
                         </b-col>
                     </b-row>
-                    <b-card-body class="text-center">
-                        <b-card-title>{{ pet.name }}</b-card-title>
-                        <b-card-sub-title>{{ pet.location }}</b-card-sub-title>
-                        <b-badge :variant="getBadgeVariant(pet.status)" class="mt-2">{{mapStatus((pet.status).toString().toLowerCase()) }}</b-badge>
-                        <div class="d-flex justify-content-center">
-                            <b-button pill variant="outline-dark-blue"
-                                class="mt-3 px-5 d-flex align-items-center justify-content-center" to="/petDetails">
-                                <span>Ver detalles</span>
-                            </b-button>
-                        </div>
-                    </b-card-body>
-                </b-card>
-            </b-col>
-        </b-row>
-        <b-row class="pt-2">
-            <b-col cols="12">
-                <b-pagination pills v-model="page" :total-rows="total" :per-page="size" align="center">
-                </b-pagination>
-            </b-col>
-        </b-row>
-    </b-container>
+                </b-col>
+            </b-row>
+            <hr class="mt-4">
+            <b-row class="mb-5">
+                <b-col v-for="pet in pets" :key="pet.id" cols="12" sm="6" lg="4" xl="3" class="mt-3">
+                    <b-card :key="pet.id" :img-src="pet.image" class="pet-card" no-body>
+                        <b-row v-if="pet.comments > 0" class="transparent absolute-position">
+                            <b-col cols="12" class="d-flex justify-content-end">
+                                <b-button
+                                    class="py-2 d-flex align-items-center relative-position custom-transparent-button"
+                                    pill v-b-tooltip.hover.left="'Ver comentarios'" @click="getComments(pet)">
+                                    <b-icon icon="chat-left-fill" font-scale="3"
+                                        class="text-dark-secondary-orange chat-icon mt-2"></b-icon>
+                                    <span class="text-light comments">
+                                        {{ (pet.comments > 9) ? '9+' : pet.comments }}
+                                    </span>
+                                </b-button>
+                            </b-col>
+                        </b-row>
+                        <b-card-body class="text-center">
+                            <b-card-title>{{ pet.name }}</b-card-title>
+                            <b-card-sub-title>{{ pet.location }}</b-card-sub-title>
+                            <b-badge :variant="getBadgeVariant(pet.status)" class="mt-2">{{
+                                        mapStatus((pet.status).toString().toLowerCase()) }}</b-badge>
+                            <div class="d-flex justify-content-center">
+                                <b-button pill variant="outline-dark-blue"
+                                    class="mt-3 px-5 d-flex align-items-center justify-content-center" to="/petDetails">
+                                    <span>Ver detalles</span>
+                                </b-button>
+                            </div>
+                        </b-card-body>
+                    </b-card>
+                </b-col>
+            </b-row>
+            <b-row class="pt-2">
+                <b-col cols="12">
+                    <b-pagination pills v-model="page" :total-rows="total" :per-page="size" align="center">
+                    </b-pagination>
+                </b-col>
+            </b-row>
+        </b-container>
+        <Modal :comments="comments" :petId="selectedPetId" :canComment="canComment"
+            :cancelRequest="selectedPetCancelRequest" @comment-added="loadComments()" />
+    </div>
 </template>
 
 <script>
@@ -82,6 +88,7 @@ import instance from "../../../../config/axios";
 import getStatusses from "../../../../kernel/data/userStatusses";
 import { statusses } from "../../../../kernel/data/mappingDictionaries";
 
+import Modal from "../components/Comments.vue";
 import gatoWalkingGif from "@/assets/imgs/gatoWalking.gif";
 
 export default {
@@ -96,7 +103,11 @@ export default {
                 searchValue: ""
             },
             statusses: getStatusses,
-            pets: []
+            pets: [],
+            selectedPetId: "",
+            selectedPetCancelRequest: "",
+            comments: [],
+            canComment: false
         }
     },
     methods: {
@@ -141,14 +152,53 @@ export default {
                 case 'approved': return 'success';
                 case 'in_revision': return 'warning';
                 case 'adopted': return 'info';
-                case 'closed': return 'danger';                
+                case 'closed': return 'danger';
                 default: return 'secondary';
             }
         },
+        async getComments(pet) {
+            try {
+                Swal.fire({
+                    title: 'Cargando...',
+                    text: 'Estamos cargando los comentarios, espera un momento',
+                    imageUrl: gatoWalkingGif,
+                    imageWidth: 160,
+                    imageHeight: 160,
+                    showConfirmButton: false
+                })
+                this.selectedPetId = pet.id                                
+                this.canComment = ['in_revision', 'approved'].includes(pet.status);
+                const response = await instance.post(`/pet/comment/all`, {
+                    pet: pet.id,
+                    user: this.form.user
+                })
+                this.comments = response.data.data
+                Swal.close()
+                this.$bvModal.show('commentsModal')
+            } catch (error) {
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Ocurrió un error al cargar los comentarios',
+                    icon: 'error',
+                    iconColor: '#A93D3D',
+                    timer: 3000,
+                    timerProgressBar: true,
+                    showConfirmButton: false
+                }).then(() => {
+                    this.$bvModal.hide('commentsModal')
+                })
+            }
+        },
+        async loadComments() {            
+            const pet = this.pets.find(pet => pet.id === this.selectedPetId);
+            if (pet) {
+                await this.getComments(pet);
+            }
+        }
     },
     mounted() {
         this.getMyPets();
-    },    
+    },
     watch: {
         page(previous, next) {
             if (previous !== next) {
@@ -156,6 +206,9 @@ export default {
             }
         }
     },
+    components: {
+        Modal
+    }
 }
 </script>
 
