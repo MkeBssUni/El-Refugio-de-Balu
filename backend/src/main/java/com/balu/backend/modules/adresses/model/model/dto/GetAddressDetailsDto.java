6aeb49bcd3c9d2e8b8e1702fdc85459781631fbb
Lisseth Fuentes <20211tn014@utez.edu.mx>
2024-04-10 23:10:33 -0600

    [FIX] fixes for save and update address methods

---
+++ backend/src/main/java/com/balu/backend/modules/adresses/model/model/dto/GetAddressDetailsDto.java
@@ -0,0 +1,14 @@
+package com.balu.backend.modules.adresses.model.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class GetAddressDetailsDto {
+    private String userId;
+}
