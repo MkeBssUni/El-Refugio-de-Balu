ca20756d082a1faf13496ec34f953b51c9403d9d
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-04-12 11:44:02 -0600

    [ADD] watch session

--- elRefugioDeBalu/src/App.vue
+++ elRefugioDeBalu/src/App.vue
@@ -4,10 +4,45 @@ export default {
   components: { DinamicNavigation },
   name: 'App',
   data() {
-    return {}
+    return {
+      userId: this.getUserIdFromStorage()
+    }
+  },
+  created() {
+    window.addEventListener('storage', this.handleStorageChange);
+    this.intervalCheck = setInterval(this.checkLocalStorage, 1000);
+  },
+  beforeDestroy() {
+    window.removeEventListener('storage', this.handleStorageChange);
+    clearInterval(this.intervalCheck);
+  },
+  methods: {
+    getUserIdFromStorage() {
+      return localStorage.getItem('userId');
+    },
+    handleStorageChange(event) {
+      if (event.key === 'userId') {
+        this.userId = this.getUserIdFromStorage();
+      }
+    },
+    checkLocalStorage() {
+      const currentUserId = this.getUserIdFromStorage();
+      if (this.userId !== currentUserId) {
+        this.userId = currentUserId;
+      }
+    },
+    redirectUser() {
+      this.$router.push("/")
+    }
+  },
+  watch: {
+    userId(newVal) {
+      if (!newVal) {
+        this.redirectUser();
+      }
+    }
   }
 }
-
 </script>
 
 <template>
